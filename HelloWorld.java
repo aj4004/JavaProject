c357d5254d8a7b2e3e3dd1883e1f9dc0d2553c47
ajay4004 <ajayyadav4004@gmail.com>
2019-07-19 13:02:19 +0530

    Added function to add

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello  World! I am here after adding webhooks");
-    
+    int num1 = 3,num2 = 4;
+    System.out.println("Adding "+ Integer.toString(num1) + " and "+ Integer.toString(num2) +" = "+ add(num1,num2) );
   }
+
+  private static int add(int a,int b){
+  	return a+b;
+  }
+
 }
+
